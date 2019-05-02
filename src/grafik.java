bb1bfe172b850b93c737d36b3cce62237ecb5e44
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-05-02 10:18:32 +0200

    "färdig" med spelet

--- src/grafik.java
+++ src/grafik.java
@@ -6,12 +6,15 @@ import java.awt.image.BufferStrategy;
 
 public class grafik extends Canvas implements Runnable {
 
-    private int mariox = 300;
-    private int marioy = 500;
+    private int mariox = 300, marioy = 500, blockX = 500, blockY = 400, groundY = 600, block2Y = 400, block2X = 350;
 
     private boolean isJumping = false;
     private boolean isFalling = false;
+    private boolean onBlock = false;
+    private boolean onGround = true;
     private int jumpEnergy = 1;
+    private boolean energyLeft = false,energyRight = false;
+
 
     private Thread thread;
     private boolean running = false;
@@ -76,6 +79,7 @@ public class grafik extends Canvas implements Runnable {
     }
 
     private void update() {
+        // Uppdaterar gubbens koordinater när man rör på sig
         if (jumpEnergy == 0) {
             isJumping = false;
             isFalling = true;
@@ -83,28 +87,115 @@ public class grafik extends Canvas implements Runnable {
         } else if (isJumping) {
             marioy-=10;
             jumpEnergy--;
+            onGround = false;
         }
-        if (marioy == 500) {
+        if (marioy+100 == groundY) {
             jumpEnergy = 1;
             isFalling = false;
+            onGround=true;
+        }
+        if (marioy <= blockY+50 && marioy+100>=blockY) {
+            if (mariox+50 == blockX) {
+                energyRight = false;
+            } else if (mariox == blockX+50) {
+                energyLeft = false;
+            }
+        } if (marioy <= block2Y+50 && marioy+100>=block2Y) {
+            if (mariox+50 == block2X) {
+                energyRight = false;
+            } else if (mariox == block2X+50) {
+                energyLeft = false;
+            }
+        } if (energyRight) {
+            mariox+=10;
+            if (!onGround && !isJumping) {
+                jumpEnergy=0;
+            }
+        } else if (energyLeft) {
+            if (mariox == 0) {
+                energyLeft = false;
+            } else {
+                mariox -= 10;
+            }
+            if (!onGround && !isJumping) {
+                jumpEnergy=0;
+            }
+        }
+        // Kollar om gubben och ett objekt kolliderar
+        if (mariox+50>blockX && mariox<blockX+50) {
+            if (marioy+100==blockY && !isJumping) {
+                jumpEnergy=1;
+                isFalling=false;
+                onBlock=true;
+            } else if (blockY+50==marioy) {
+                jumpEnergy=0;
+                blockY = blockY+1000;
+            }
+        } else if (mariox+50<=block2X || mariox>=block2X+50 || !onGround) {
+            onBlock=false;
+        } if (mariox+50>block2X && mariox<block2X+50) {
+            if (marioy+100==block2Y && !isJumping) {
+                jumpEnergy=1;
+                isFalling=false;
+                onBlock=true;
+            } else if (block2Y+50==marioy) {
+                jumpEnergy=0;
+                block2Y = block2Y+1000;
+            }
+        } else if (mariox+50<=block2X || mariox>=block2X+50 || !onGround) {
+            onBlock=false;
         }
 
     }
 
     private void draw(Graphics g) {
+        // Ritar ut bilden
         drawBackground(g);
         drawGround(g);
         drawMario(g);
+        drawBlock(g, blockX, blockY);
+        drawBlock(g,block2X,block2Y);
+    }
+
+    private void drawBlock(Graphics g, int x, int y) {
+        g.setColor(new Color(0x8B4423));
+        g.fillRect(x,y,50,50);
+        g.setColor(new Color(0x00000));
+        g.fillRect(x,y+47,50,3);
+        g.fillRect(x,y+11,50,3);
+        g.fillRect(x,y+23,50,3);
+        g.fillRect(x,y+36,50,3);
+        g.fillRect(x+21,y,3,12);
+        g.fillRect(x+47,y,3,12);
+        g.fillRect(x+10,y+11,3,12);
+        g.fillRect(x+39,y+11,3,12);
+        g.fillRect(x+21,y+24,3,12);
+        g.fillRect(x+47,y+24,3,12);
+        g.fillRect(x+10,y+37,3,12);
+        g.fillRect(x+39,y+37,3,12);
     }
 
     private void drawBackground(Graphics g) {
-        g.setColor(new Color(0xffffff));
+        g.setColor(new Color(0x25AAFF));
         g.fillRect(0, 0, 10000, 600);
+        g.setColor(new Color(0xFFFFFF));
+        g.fillRect(100,200,100,50);
+        g.fillRect(600,180,90,50);
+        g.fillRect(280,240,130,60);
+        g.fillRect(940,340,100,50);
+        g.fillRect(1320,80,120,80);
+        g.fillRect(1650,360,170,30);
     }
 
     private void drawGround(Graphics g) {
         g.setColor(new Color(0x8B4513));
         g.fillRect(0,600,2000,600);
+        g.setColor(new Color(0xC7430F));
+        g.fillRect(0,600,2000,50);
+        g.setColor(new Color(0x000000));
+        for (int i = 0 ; i < 50 ; i++) {
+            g.fillRect(i*50,600,3,50);
+        }
     }
 
     private void drawMario(Graphics g) {
@@ -122,16 +213,17 @@ public class grafik extends Canvas implements Runnable {
         @Override
         public void keyPressed(KeyEvent keyEvent) {
             if (keyEvent.getKeyChar()=='a') {
-                if (mariox > 0) {
-                    mariox-=5;
-                } else if (mariox == 0) {
-                }
+                    energyLeft = true;
+
             } else if (keyEvent.getKeyChar()=='d') {
-                mariox+=5;
+                    energyRight =true;
+
+
             } else if (keyEvent.getKeyChar()=='w'){
+
                 if (!isJumping && !isFalling) {
                     isJumping = true;
-                    jumpEnergy = 10;
+                    jumpEnergy = 25;
                 }
             }
 
@@ -139,7 +231,11 @@ public class grafik extends Canvas implements Runnable {
 
         @Override
         public void keyReleased(KeyEvent keyEvent) {
-
+            if (keyEvent.getKeyChar()=='a') {
+                energyLeft = false;
+            } else if (keyEvent.getKeyChar()=='d') {
+                energyRight = false;
+            }
         }
     }
 
